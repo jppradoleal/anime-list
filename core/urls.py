4fdc61da462da670b86a28967d8ac3dc369a1d56
Joao <joao.prado@buser.com.br>
2022-07-30 10:18:20 -0300

    feat: inits debug routes
    feat: installs flake8
    
    Signed-off-by: Joao <joao.prado@buser.com.br>

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http.response import HttpResponse
+
+
+def debug(request):
+    return HttpResponse('Working!')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('debug/', debug),
+    path('api/', include('animes.urls'))
 ]
